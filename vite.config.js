f4bc114f5227ffb538d6e763694ad0b6138e95ac
GPLEADER <axrorxonovfirdavs@gmail.com>
2025-04-19 14:13:07 +0300

    oxshagandir

--- vite.config.js
+++ vite.config.js
@@ -5,29 +5,38 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-    manifest: {
-      name: 'Safros Hisobot',
-      short_name: 'Safros',
-      description: 'Hisobotlar tizimi',
-      theme_color: '#ffffff',
-      background_color: '#ffffff',
-      display: 'standalone',
-      start_url: '/',
-      icons: [
-        {
-          src: 'icons/logo.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'icons/logo.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Safros Hisobot',
+        short_name: 'Safros',
+        description: 'Hisobotlar tizimi',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/fontawesome-svg-core']
     }
-  })],
+  }
 })
