5c9ec4f480d61437acfcd6b96c7e6b321b1e06de
GPLEADER <axrorxonovfirdavs@gmail.com>
2025-04-20 13:52:19 +0300

    Settings ozgardi

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -13,8 +13,16 @@ function Settings() {
   }
 
   return (
-    <div className='pt-16 px-4'>
-      <button onClick={handleNavigateToLogin} className='px-2 py-3 bg-red-500 text-white flex items-center gap-2 rounded-lg'><FontAwesomeIcon icon={faDoorOpen} />Chiqish</button>
+    <div className='pt-17 px-4'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-2'>
+          <div className='w-12 h-12 rounded-full bg-gray-300 flex items-center justify-center'>
+            <span className='text-lg'>G</span>
+          </div>
+          <p className='text-xl'>Guest</p>
+        </div>
+        <button onClick={handleNavigateToLogin} className='px-3 py-2 bg-red-500 text-white flex items-center gap-2 rounded-lg'><FontAwesomeIcon icon={faDoorOpen} />Chiqish</button>
+      </div>
     </div>
   )
 }
