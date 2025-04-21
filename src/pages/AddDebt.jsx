7edab570739f7aba8a6982c7da4d1cffbaf4b7b7
GPLEADER <axrorxonovfirdavs@gmail.com>
2025-04-21 18:01:15 +0300

    AddDebt qoshildi

--- src/pages/AddDebt.jsx
+++ src/pages/AddDebt.jsx
@@ -12,6 +12,43 @@ function AddDebt() {
           <span className='text-2xl'>Ortga</span>
         </Link>
       </div>
+
+      <div className='pt-16 px-4'>
+        <div className='flex flex-col justify-center gap-y-3'>
+          <input
+            id="ism"
+            name="ism"
+            type="text"
+            require
+            placeholder='Ism'
+            autoComplete="ism"
+            className="mt-1 block w-full rounded-md bg-white px-3 py-2 text-sm text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600"
+          />
+          <textarea
+            id="desc"
+            name="desc"
+            require
+            placeholder='Izoh'
+            autoComplete="desc"
+            className="mt-1 block w-full rounded-md bg-white px-3 py-2 text-sm text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600"
+          />
+
+          <div className='flex items-center gap-1 justify-between'>
+            <button className='bg-indigo-600 px-4 max-w-23 w-full py-2 rounded-md text-white '>UZS</button>
+            <button className='bg-indigo-600 px-4 max-w-23 w-full py-2 rounded-md text-white '>USD</button>
+            <button className='bg-indigo-600 px-4 max-w-23 w-full py-2 rounded-md text-white '>UZS X/R</button>
+          </div>
+          <input
+            id="count"
+            name="count"
+            type="number"
+            require
+            placeholder='summa'
+            autoComplete="count"
+            className="mt-1 block w-full rounded-md bg-white px-3 py-2 text-sm text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600"
+          />
+        </div>
+      </div>
     </div>
   )
 }
